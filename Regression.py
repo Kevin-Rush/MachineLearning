7527d130e6053825a8ede2a81e539fdef9d86370
Kevin Rush <k.rush@sap.com>
2019-01-30 18:48:54 +0000

    Linear Regression with functions, began creating our own Linear Regression function

--- Regression.py
+++ Regression.py
@@ -1,41 +1,85 @@
 #regression simulating stock prices
 
 import pandas as pd
-import quandl, math
+import quandl, math, datetime
 import numpy as np
 from sklearn import preprocessing, model_selection, svm # this is for scaling features, it is normally good to have feature between 1 and -1 
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt 
+from matplotlib import style
+import pickle 
 
-df = quandl.get("WSE/TBULL", authtoken="xykwYQEpNJg8xpdTbLGH")
+style.use('ggplot')
+
+df = quandl.get("WIKI/GOOGL", authtoken="xykwYQEpNJg8xpdTbLGH")
+
+#print(df)
+print()
 
 df = df[['Open', 'High', 'Low', 'Close', 'Volume']]
 
-df['HL_PCT'] = (df['High'] - df['Close'])/df['Close']*100
-df['PCT_Change'] = (df['Close'] - df['Open'])/df['Open']*100
+df['HL_PCT'] = (df['High'] - df['Close'])/df['Close']*100       #percent change between the high and the close
+df['PCT_Change'] = (df['Close'] - df['Open'])/df['Open']*100    #percent change between open and close
 
-df = df[['Close','HL_PCT','PCT_Change','Volume']]   #these will be features
+df = df[['Close','HL_PCT','PCT_Change','Volume']]               #these will be features
 
 #what is the label? we are trying to predict the price
 
-forcast_col = 'Close'
+forcast_col = 'Close'                               #forcast column = the close price
 df.fillna(-99999, inplace=True)                     #can't let there be unfilled data therefore fill empty space with True
 forcast_out = int(math.ceil(0.01*len(df)))          #we are going to try to predict out 10% of the Data Frame
 
-print(forcast_out)
+#print(forcast_out)
 
 df['label'] = df[forcast_col].shift(-forcast_out)   #we are looking at the features to try to predict the Close in '10 days' (actually 10%)
-df.dropna(inplace=True)
+
 
 X = np.array(df.drop(['label'],1))                  # will define features
-y = np.array(df['label'])                           # will define leabels
-
 X = preprocessing.scale(X)
-y = np.array(df['label'])
+X = X[:-forcast_out]                                # will define leabels
+X_lately = X[-forcast_out:]
 
-X_train, X_test, y_train, y_test = model_selection.train_test_split(X,y,test_size=0.2)
+#print(X)
+#print(X_lately) 
 
-clf = LinearRegression(n_jobs=-1)
-clf.fit(X_train, y_train)
+df.dropna(inplace=True)
+y = np.array(df['label'])    
+
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X,y,test_size=0.2)  #only sending 20% of data??
+
+# clf = LinearRegression(n_jobs=-1)
+# clf.fit(X_train, y_train)
+# with open('linearregression.pickle', 'wb') as f: #we are now saving the classifier so we can avoid the training step b/c we can save the classifier after it's been trained
+#     pickle.dump(clf, f)                            #should still retrain it every now and again, but this way we won't retrain everytime we run the program
+
+pickle_in = open('linearregression.pickle', 'rb')
+clf = pickle.load(pickle_in)
+
+
+
 accuracy = clf.score(X_test, y_test)
 
-print(accuracy)
+
+forcast_set = clf.predict(X_lately)         #this is where the prediction is happening 
+
+print(forcast_set, accuracy, forcast_out)
+
+df['Forcast'] = np.nan
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forcast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns) - 1)] + [i] #this is a list of values that are np.nan  + i (i is the forcast) 
+
+print(df.tail())
+
+df ['Close'].plot() 
+df['Forcast'].plot() 
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show() 
